a23fbfea2b6424372072544843cdef530e996194
Bobby350 <84213759+Bobby350@users.noreply.github.com>
2021-12-11 12:41:37 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,35 @@
+
+const firebaseConfig = {
+      apiKey: "AIzaSyDh63Zz_I6urjPfSIL-6EBOQG_P3F9nXec",
+      authDomain: "kwitter-3662a.firebaseapp.com",
+      databaseURL: "https://kwitter-3662a-default-rtdb.firebaseio.com",
+      projectId: "kwitter-3662a",
+      storageBucket: "kwitter-3662a.appspot.com",
+      messagingSenderId: "821791431609",
+      appId: "1:821791431609:web:36e9399fea3a9f9689f468"
+    };
+    
+    // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
+
+function logout() {
+      user_name = document.getElementById("user_name").ariaValueMax;
+      localStorage.setItem("user_name", user_name);
+      window.location = "index.html"
+  }
+
+
+  function addroom() {
+      user_name = document.getElementById("user_name").value;
+      firebase.database().ref("/").child(user_name).update({
+          grade: "5th"
+      })
+  }
